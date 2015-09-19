fec0c6d03ec37b623a05e08aebde2a4141d0e141
Livvie Lin <livvie.lin@gmail.com>
2015-09-19 14:53:22 -0700

    Removes code routing from app.js

--- client/app/app.js
+++ client/app/app.js
@@ -23,10 +23,6 @@ angular.module('shortly', [
       templateUrl: 'app/shorten/shorten.html',
       controller: 'ShortenController'
     })
-    .when('/:code', {
-      templateUrl: 'app/shorten/shorten.html',
-      controller: 'ShortenController'
-    })
     .otherwise('/links');
 
     // We add our $httpInterceptor into the array
